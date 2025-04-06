7c14c2724a0fd9489f66fe765ddf7f972f3f31b1
Himanshimalviya123 <malviyahimanshi14@gmail.com>
2025-04-06 21:42:51 +0530

    modify

---
+++ src/App.jsx
@@ -0,0 +1,52 @@
+// ==========================( expression in js )======================================================
+// const name="himanshi malviya"
+// const age=20
+// const city="bhopal"
+// const salary=35000
+//  const app=()=>{
+//   return(
+//     <>
+// <h1>My name is {name}.i ame {age} year old.i am live in {city}.my salary is{salary}
+//    </h1>
+//     </>
+//   )
+//  }
+//  export default app;
+// ==============================(inserting  a large block of element)=============================
+// const sub=
+// <ul>
+//   <li>php</li>
+//   <li>oracle</li>
+//   <li>java</li>
+
+// </ul>
+// const subject=()=>{
+//   return(
+// <>
+// <h1>my subject is:{sub}</h1>
+// </>
+//   )
+// }
+// export default subject;
+// ==============================( one top level element )================================
+// let abc=()=>{
+//   return(
+//     <p style={{background:"green"}}>
+//     <h4>hello cybrom</h4>  <br/>  <h4>e learn react js</h4>
+//     <h4>my subject is mern full stack .</h4>
+// </p>
+//   )
+// }
+// export default abc;
+// ====================(element must be closed )====================================
+// const app=()=>{
+//   return(
+//     <>
+//     <h1>
+//      <b><u> we are learn is react js</u></b>
+//     </h1>
+//     </>
+//   )
+// }
+// export default app;
+// ====================( attribute class=className: )========================
