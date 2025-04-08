3c07e8c1e17e7867ce82f42d69964d6d4a347cea
Himanshimalviya123 <malviyahimanshi14@gmail.com>
2025-04-08 18:29:22 +0530

    modify

--- src/App.jsx
+++ src/App.jsx
@@ -129,37 +129,37 @@
 //     )
 // }
 // export default App;
-// ===================create a counter app===============================
-import { useState } from "react";
+// ===================create a counter app=====(use state)==========================
+// import { useState } from "react";
 
-const App=()=>{
-    const[cnt,setCnt]=useState(0);
-   const mydec=()=>{
-    if  (setCnt<=0)
-{
-    alert("not less than zero!")
-}
-    else
-    {
-        setCnt(cnt-1);
-    }
-}
-    return(
-        <>
-        <h1 >my counter app{name}</h1>
-        <div style={{border:"2px solid red",padding:"20px",borderRadius:"30px",
-            backgroundColor:"blue",width:"400px",margin:"auto"}}>
-                <h1>my count :{cnt}</h1>
-                <button onClick={()=>{setCnt(cnt+1)}}> increment</button>
-                <button onClick={()=>{mydec}}> decrement</button>
-                <button onClick={()=>{setCnt(0)}}>reset</button>
+// const App=()=>{
+//     const[cnt,setCnt]=useState(0);
+//    const mydec=()=>{
+//     if  (cnt<=0)
+// {
+//     alert("not less than zero!")
+// }
+//     else
+//     {
+//         setCnt(cnt-1);
+//     }
+// }
+//     return(
+//         <>
+//         <h1 >my counter app</h1>
+//         <div style={{border:"2px solid red",padding:"20px",borderRadius:"30px",
+//             backgroundColor:"lightskyblue",width:"400px",margin:"auto"}}>
+//                 <h1>my count :{cnt}</h1>
+//                 <button onClick={()=>{setCnt(cnt+1)}}> increment</button>
+//                 <button onClick={mydec}> decrement</button>
+//                 <button onClick={()=>{setCnt(0)}}>reset</button>
              
-        </div>
-        </>
-    )
-}
-export default App;
-// =========================================
+//         </div>
+//         </>
+//     )
+// }
+// export default App;
+// ============================== ( no.increasing )=====(useEffect=side effect)======
 // import { useState,useEffect } from "react";
 // const App=()=>{
 //     const[count,setCount]=useState(0);
@@ -176,3 +176,110 @@ export default App;
 //     )
 // }
 // export default App;
+// ==========================================(useaeffect 1)====================================================================
+// import { useState,useEffect } from "react";
+// const App=()=>{
+//     const[count,setCount]=useState(0);    //variable
+//     useEffect(()=>{
+//         setTimeout(()=>{
+//             setCount(count+1)
+//         },5000)
+//     })
+//     return(
+//         <>
+//         <h1>react example :{count}</h1>
+       
+//         </>
+//     )
+// }
+// export default App;
+// ==================================(useeffect 2)=======================================
+// import { useState,useEffect } from "react";
+// const App=()=>{
+//     const[count,setCount]=useState(0);    //variable
+//     useEffect(()=>{
+//         setTimeout(()=>{
+//             setCount(count+1)
+//         },5000)
+//     },[])
+//     return(
+//         <>
+//         <h1>react example :{count}</h1>
+       
+//         </>
+//     )
+// }
+// export default App;
+// =================================(useeffect-3)================================
+// import { useState,useEffect } from "react";
+// const App=()=>{
+//     const[count,setCount]=useState(0);    //variable
+//     const[multi,setMulti]=useState(0);
+//     useEffect(()=>{
+     
+//           setMulti(count*2)
+//     },[count])
+//     return(
+//         <>
+//         <h1>my counter :{count}</h1>
+//         <h1>multiplication :{multi}</h1>
+//         <button onClick={()=>{setCount(count+1)}}>click here!</button>
+//         </>
+//     )
+// }
+
+// export default App;
+
+// ======================================form===============================
+// import { useState} from "react";
+// const App=()=>{
+//     const[name,setName]=useState("");
+//     const[city,setCity]=useState("");
+//     const handleSubmit=()=>{
+//         alert(`my name :${name} city:${city}`)
+//     }
+//     return(
+// <>
+// <h1>application form</h1>
+// name:<input type="text" value={name}
+// onChange={(e)=>{setName(e.target.value)}}/>
+// <br/>
+// city:<input type="text" value={city}
+// onChange={(e)=>{setCity(e.target.value)}}/>
+// <br/>
+// <button onClick={handleSubmit}>save!</button>
+// </>
+//     )
+// }
+// export default App;
+// ==================================spread operter====================
+import { useState} from "react";
+const App=()=>{
+    const[input,setInput]=useState({});  //input={}
+    const handleInput=(e)=>{
+        let name=e.target.name;
+        let value=e.target.value;
+        setInput(values=>({...values,[name]:value}))
+        console.log(input);
+        
+    }
+    return(
+<>
+<h1>application form</h1>
+name:<input type="text" name="name"
+onChange={handleInput}/>
+<br/>
+rollno.:<input type="text" name="rollno."
+onChange={handleInput}/>
+<br/>
+city:<input type="text" name="city"
+onChange={handleInput}/>
+<br/>
+fees:<input type="text" name="fees"
+onChange={handleInput}/>
+<br/>
+<button onClick={handleInput}>save!</button>
+</>
+    )
+}
+export default App;
